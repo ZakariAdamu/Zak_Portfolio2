a1d206d5a486561a695ec3609832fe7d18afa837
Zakari <ojonimizakari@gmail.com>
2020-12-25 17:06:14 +0100

    project completed but needs more touches

---
+++ main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate( keyframes = {
+            scrollTop: $($(this).attr('href')).offset().top -100
+        }, options = 2000);
+    })
+
+    $('#up').on('click', function () {
+        $('html, body').animate( keyframes = {
+            scrollTop: 0
+        }, options = 1500)
+    })
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once:false
+    });
+
+    /* ===============================  Mouse effect  =============================== */
+
+function mousecursor() {
+    if ($("body")) {
+        const e = document.querySelector(".cursor-inner"),
+            t = document.querySelector(".cursor-outer");
+        let n, i = 0,
+            o = !1;
+        window.onmousemove = function (s) {
+            o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+        }, $("body").on("mouseenter", "a, .cursor-pointer", function () {
+            e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
+        }), $("body").on("mouseleave", "a, .cursor-pointer", function () {
+            $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
+        }), e.style.visibility = "visible", t.style.visibility = "visible"
+    }
+};
+
+$(function () {
+    mousecursor();
+});
+})
